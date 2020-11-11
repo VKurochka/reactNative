1bb847438820718441d2405c8e88816899db6bec
Viktor Kurochka <viktor_kurochka@epam.com>
2020-11-11 15:08:52 +0200

    Delete some content

--- App.tsx
+++ App.tsx
@@ -53,11 +53,6 @@ const App = () => {
                 <DebugInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
