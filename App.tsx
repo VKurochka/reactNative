c678113e5af3ff127e28b26c18386b1ff7b1b8d9
Viktor Kurochka <viktor_kurochka@epam.com>
2020-11-11 15:07:14 +0200

    Test comitt hook

--- App.tsx
+++ App.tsx
@@ -54,7 +54,6 @@ const App = () => {
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
               <Text style={styles.sectionDescription}>
                 Read the docs to discover what to do next:
               </Text>
